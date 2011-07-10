47f43f70b1e792a8610cbb6ceb1435fd96448ba9
Freddie FUJIWARA <github@zeg.la>
2011-07-10 19:05:21 +0000

    crwl -> crawl

--- app/controllers/texts_controller.php
+++ app/controllers/texts_controller.php
@@ -8,5 +8,9 @@ class TextsController extends AppController {
 		$this->set('text', $this->TextCheckAction -> getRangeData(1,1,1,3));
 	}
 
+	function crawl() {
+		$this->set('count', $this->Text->crawl());
+	}
+
 }
 ?>
