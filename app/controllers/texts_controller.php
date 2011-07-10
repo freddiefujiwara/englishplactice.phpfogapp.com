01aab5a414f5b0de1b8cc0e7da02fa574c3ac284
Freddie FUJIWARA <github@zeg.la>
2011-07-10 19:47:36 +0000

    debug

--- app/controllers/texts_controller.php
+++ app/controllers/texts_controller.php
@@ -5,14 +5,18 @@ class TextsController extends AppController {
 	var $uses = array("Text","TextCheckAction");
 
 	function index() {
-		$this->set('text', $this->TextCheckAction -> getRangeData(
+		$text = $this->TextCheckAction -> getRangeData(
 			intval($this -> params["start"]),
 			intval($this -> params["end"]),
 			intval($this -> params["step"]),
 			intval($this -> params["questions"])
-		));
-
+		)
+		if(array_key_exists("indexes",$this -> data)){
+			$text["indexes"] = explode(",",$this -> data["indexes"]);
+		}
+		$this->set('text', $text);
 	}
+
 
 	function crawl() {
 		$this->set('count', $this->Text->crawl());
