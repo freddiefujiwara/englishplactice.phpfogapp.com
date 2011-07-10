7d1f31e2c90982327c0618cd0d65f8762c5899dd
Freddie FUJIWARA <github@zeg.la>
2011-07-10 23:26:54 +0000

    debug

--- app/models/text_check_action.php
+++ app/models/text_check_action.php
@@ -40,11 +40,11 @@ class TextCheckAction extends AppModel {
 				'noBlank' => array(
 					'rule' => array('notEmpty'),
 					'required' => true,
-					'message'  => ""
+					'message'  => " "
 				),
 				'equalTo' => array(
 					'rule' => array('equalTo',$splitted[$index]),
-					'message'  => ""
+					'message'  => " "
 				)
 			);
 		}
