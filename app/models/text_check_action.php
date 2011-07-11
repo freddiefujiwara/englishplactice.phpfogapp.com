5e17139a1356ddb322409aee4fbabe901e60a72e
Freddie FUJIWARA <github@zeg.la>
2011-07-11 01:03:43 +0000

    debug

--- app/models/text_check_action.php
+++ app/models/text_check_action.php
@@ -11,7 +11,7 @@ class TextCheckAction extends AppModel {
 		"ours","yours","theirs",
 		"are","am","is","were","was",
 		"and",
-		"bob","naomi"
+		"bob","naomi","nick"
 	);
 	var $validate = array();
 	function getRangeData($start,$end,$step,$questions){
