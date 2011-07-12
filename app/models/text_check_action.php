fc9ca9206134305d97fda72eea223eb699aedeab
Freddie FUJIWARA <github@zeg.la>
2011-07-12 23:44:49 +0000

    ngList added

--- app/models/text_check_action.php
+++ app/models/text_check_action.php
@@ -11,7 +11,7 @@ class TextCheckAction extends AppModel {
 		"ours","yours","theirs",
 		"are","am","is","were","was",
 		"and",
-		"bob","naomi","nick","dave","jennifer","lisa","jane","mike","joe"
+		"bob","naomi","nick","dave","jennifer","lisa","jane","mike","joe","panama"
 	);
 	var $validate = array();
 	function getRangeData($start,$end,$step,$questions){
